c6673ef813a903b6fad001c66adaac93668fdc83
kim980onit <kimanikamau971@gmail.com>
2024-01-18 01:12:34 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    power = []
+    for line in matrix:
+      row_ power = [pow(c,2) for c in line]
+      power.append(row_power)
+   return power     
